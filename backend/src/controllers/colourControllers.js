b6290af2c2a7c863d2b3277ffc8e07456a6b1145
Francois Chavallard <francois.chavallard@gmail.com>
2023-06-28 19:18:59 +0200

    création d'un manager et de controllers pour chaque entité des paramètres

---
+++ backend/src/controllers/colourControllers.js
@@ -0,0 +1,91 @@
+const models = require("../models");
+
+const browse = (req, res) => {
+  models.colour
+    .findAll()
+    .then(([rows]) => {
+      res.send(rows);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const read = (req, res) => {
+  models.colour
+    .find(req.params.id)
+    .then(([rows]) => {
+      if (rows[0] == null) {
+        res.sendStatus(404);
+      } else {
+        res.send(rows[0]);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const edit = (req, res) => {
+  const colour = req.body;
+
+  // TODO validations (length, format...)
+
+  colour.id = parseInt(req.params.id, 10);
+
+  models.colour
+    .update(colour)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const add = (req, res) => {
+  const colour = req.body;
+
+  // TODO validations (length, format...)
+
+  models.colour
+    .insert(colour)
+    .then(([result]) => {
+      res.location(`/colours/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const destroy = (req, res) => {
+  models.colour
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  browse,
+  read,
+  edit,
+  add,
+  destroy,
+};
---
+++ backend/src/models/NetworkManager.js
@@ -0,0 +1,22 @@
+const AbstractManager = require("./AbstractManager");
+
+class NetworkManager extends AbstractManager {
+  constructor() {
+    super({ table: "network" });
+  }
+
+  insert(item) {
+    return this.database.query(`insert into ${this.table} (title) values (?)`, [
+      item.title,
+    ]);
+  }
+
+  update(item) {
+    return this.database.query(
+      `update ${this.table} set title = ? where id = ?`,
+      [item.title, item.id]
+    );
+  }
+}
+
+module.exports = NetworkManager;
