88cd7ae990a6b032d9ed398a0c70f83905f57e07
LightQv <QVivian23@gmail.com>
2023-06-28 18:30:41 +0200

    login backend done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,33 +1,33 @@
-import { useFormik } from "formik";
+// import { useFormik } from "formik";
 // import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import logo from "../assets/images/logo.svg";
-import { useUserContext } from "../contexts/UserContext";
+// import { useUserContext } from "../contexts/UserContext";
 import "react-toastify/dist/ReactToastify.css";
-import { loginSchema } from "../services/validators";
+// import { loginSchema } from "../services/validators";
 // import { notifyError } from "../services/ToastNotificationService";
 // import FormError from "../components/FormError";
 // import APIService from "../services/APIService";
 import styles from "./Login.module.css";
 
 export default function Login() {
-  const { login } = useUserContext();
-  const navigate = useNavigate();
+  // const { login } = useUserContext();
+  // const navigate = useNavigate();
 
   // Formik Logic
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
+  // const formik = useFormik({
+  //   initialValues: {
+  //     email: "",
+  //     password: "",
+  //   },
 
-    validationSchema: loginSchema,
+  //   validationSchema: loginSchema,
 
-    onSubmit: (values) => {
-      console.log(values);
-    },
-  });
+  //   onSubmit: (values) => {
+  //     console.log(values);
+  //   },
+  // });
   return (
     <main className={styles.page}>
       <form action="login" className={styles.formWrapper}>
