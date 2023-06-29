bdad911365ab8dfe3624512b58b4b353451cd16a
LightQv <QVivian23@gmail.com>
2023-06-29 09:05:35 +0200

    login done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 import RequireAuth from "./components/routes/RequireAuth";
 import Login from "./pages/Login";
-import "./App.css";
+// import Navbar from "./components/Navbar";
 import Calculator from "./pages/Calculator";
 import FAQ from "./pages/FAQ";
 import Parameters from "./pages/Parameters";
 import Users from "./pages/Users";
+import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      {/* Public routes */}
-      <Route path="/" element={<Login />} />
-      <Route path="/calc" element={<Calculator />} />
-      <Route path="/faq" element={<FAQ />} />
+    <>
+      {/* <Navbar /> */}
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<Login />} />
+        <Route path="/calc" element={<Calculator />} />
+        <Route path="/faq" element={<FAQ />} />
 
-      {/* Admin routes */}
-      <Route element={<RequireAuth allowedRoles="Admin" />}>
-        <Route path="/params" element={<Parameters />} />
-        <Route path="/users" element={<Users />} />
-      </Route>
-    </Routes>
+        {/* Admin routes */}
+        <Route element={<RequireAuth allowedRoles="Admin" />}>
+          <Route path="/params" element={<Parameters />} />
+          <Route path="/users" element={<Users />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
