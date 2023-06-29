d574634fcfe41bfcceaeba29f541702606a1b518
Francois Chavallard <francois.chavallard@gmail.com>
2023-06-29 18:47:46 +0200

    tooltips opérationnels

--- frontend/src/components/Dropzone.jsx
+++ frontend/src/components/Dropzone.jsx
@@ -1,8 +1,12 @@
 import React, { useRef } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
+// import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import Tooltip from "@mui/material/Tooltip";
+// import { styled } from "@mui/material/styles";
 import styles from "./Dropzone.module.css";
 import button from "../assets/icons/bouton.svg";
+import info from "../assets/icons/info.png";
 
 function Dropzone({ parameter, setNotification }) {
   const inputRef = useRef({ parameter });
@@ -38,6 +42,50 @@ function Dropzone({ parameter, setNotification }) {
     label = "Indice de prix";
   }
 
+  let tooltip;
+  if (parameter === "brand") {
+    tooltip = `Chaque ligne du fichier .csv doit être au format 'Marque,Coefficient' (ex: 'Apple,1.5')`;
+  }
+  if (parameter === "model") {
+    tooltip = `Chaque ligne du fichier .csv doit être au format 'Modèle,Coefficient' (ex: 'iphone X,1.2')`;
+  }
+  if (parameter === "ram") {
+    tooltip = `Chaque ligne du fichier .csv doit être au format 'Taille,Valeur' (ex: '8,50')`;
+  }
+  if (parameter === "storage") {
+    tooltip = `Chaque ligne du fichier .csv doit être au format 'Taille,Valeur' (ex: '512,70')`;
+  }
+  if (parameter === "colour") {
+    tooltip = `Chaque ligne du fichier .csv doit être au format 'Couleur,Coefficient' (ex: 'Noir,1.3')`;
+  }
+  if (parameter === "state") {
+    tooltip = `Chaque ligne du fichier .csv doit être au format 'Etat,Pondération' (ex: 'Réparé,20')`;
+  }
+  if (parameter === "network") {
+    tooltip = `Chaque ligne du fichier .csv doit être au format 'Réseau,Valeur' (ex: '5G,50')`;
+  }
+  if (parameter === "screen") {
+    tooltip = `Chaque ligne du fichier .csv doit être au format 'Taille,Valeur' (ex: '4,20')`;
+  }
+  if (parameter === "categories") {
+    tooltip = `Chaque ligne du fichier .csv doit être au format 'Grade,Valeur min, Valeur max' (ex: 'A,280,374')`;
+  }
+  if (parameter === "price_index") {
+    tooltip = `Chaque ligne du fichier .csv doit être au format 'Indice' (ex: '0.25')`;
+  }
+
+  // const HtmlTooltip = styled(({ className, ...props }) => (
+  //   <Tooltip {...props} classes={{ popper: className }} />
+  // ))(({ theme }) => ({
+  //   [`& .${tooltipClasses.tooltip}`]: {
+  //     backgroundColor: "#f5f5f9",
+  //     color: "rgba(0, 0, 0, 0.87)",
+  //     maxWidth: 220,
+  //     fontSize: theme.typography.pxToRem(12),
+  //     border: "1px solid #dadde9",
+  //   },
+  // }));
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!inputRef.current.files[0]) {
@@ -68,7 +116,12 @@ function Dropzone({ parameter, setNotification }) {
       onSubmit={(e) => handleSubmit(e)}
     >
       <label className={styles.label} htmlFor={parameter}>
-        <p>{`${label} :`}</p>
+        <p>
+          <Tooltip title={tooltip} placement="top">
+            <img src={info} className={styles.info} alt="info" />
+          </Tooltip>
+          {` ${label} :`}
+        </p>
         <div className={styles.inputs}>
           <input
             ref={inputRef}
